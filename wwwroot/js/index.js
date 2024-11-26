49d21bfc2ab7c7db97ecceb81eff8ccac9442593
Vinicius Bernardo <vivianepolimeno@gmail.com>
2024-11-26 14:55:14 -0300

    complemetando o codigo da bibliotec no site

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,8 +1,4 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-function mostraMenu() {
+﻿function mostraMenu() {
     document.getElementById("navbar").classList.toggle("mostra-menu");
     document.getElementById("menu-hamburger").classList.toggle("menu-hamburger-rotate");
 }
